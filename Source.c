d1c162789c74d82d63644193248206007917928b
Alina <Alina@DESKTOP-NVJHBH6>
2021-03-13 22:06:46 +0200

    init

---
+++ Source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//EX1
+int* rebuild_array_min(int* arr, int n);
+
+void main() {
+
+	int arr[] = { 10, 10, 20, 30, 30, 30, 42, 42, 42, 51, 64, 64, 77 }; //EX1
+	
+
+
+}
+
+//EX1
+int* rebuild_array_min(int* arr, int n)
+{
+	return arr;
+}
