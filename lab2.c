01ae7ff2f2bc797b3b66ca94356fc2c14fcc242f
Alina <Alina@DESKTOP-NVJHBH6>
2021-03-30 22:53:22 +0300

    Q1 done

--- lab2.c
+++ lab2.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int* even_odd(int* a, int n, int **a_odd, int *p_even_size, int *p_odd_size);
+void print_arr2(int a[], int n);
 
 void lab2() {
 
@@ -10,17 +11,17 @@ void lab2() {
 
 	int arrQ1[] = { 6 , 57 , 14 , 21 , 11 , 3, 22, 42 , 9 , 15 };
 	int N = 10;
-	int* odd_array = NULL;
+	int* odd_array;
 	int odd_size = 0; 
 	int even_size = 0;
-	int* even_array = even_odd(arrQ1, N, odd_array, &even_size, &odd_size);
+	int* even_array = even_odd(arrQ1, N, &odd_array, &even_size, &odd_size);
 
-	printf("even %d od %d", even_size, odd_size);
+	printf("even %d, od %d\n\n", even_size, odd_size);
 
 	printf("first (even) array: \n");
-	print_arr(even_array, even_size);
+	print_arr2(even_array, even_size);
 	printf("\nsecond (odd) array: \n");
-	print_arr(odd_array, odd_size);
+	print_arr2(odd_array, odd_size);
 	
 	free(odd_array);
 	free(even_array);
@@ -37,15 +38,17 @@ int* even_odd(int* a, int n, int** a_odd, int* p_even_size, int* p_odd_size) {
 	}
 
 	int* a_even = (int*)calloc(*p_even_size, sizeof(int));
-	**a_odd = (int*)calloc(*p_odd_size, sizeof(int));
+	int* odd = (int*)calloc(*p_odd_size, sizeof(int));
 
 	for (int i = 0, e = 0, o = 0; i < n; i++) {
 
 		if (a[i] % 2 == 0) { a_even[e] = a[i]; e++; }
-		else { a_odd[o] = a[i]; o++; };
+		else { odd[o] = a[i]; o++; };
 
 	}
 
+	*a_odd = odd;
+	return a_even;
 
 };
 
