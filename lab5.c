cce8f08148389c2039c4e52b95472dabe074fda9
Alina-Sul <alina.sulkovsky@gmail.com>
2021-04-13 00:29:58 +0300

    students array - in progress

--- lab5.c
+++ lab5.c
@@ -6,7 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM 3
+
 //Q1
+
 int* Input_Data(int num_of_exams); //return array of grades
 double Student_Average(int* grades_array);// return Average grade of the recieved array
 double Total_Average(int* all_avg);//recieve dynamic arrray of all averages and calculates total average
@@ -15,11 +18,39 @@ void Classification(student_init_data* init_students_data, int size_init_student
 void Print_Tab(statistics); //print an elemnt of type 'statistics'
 void Free_Memory(int *ptr_arr, int size_ptr_arr);
 
+//HELPERS
 
-
-
+void Print_Tab(statistics stats); //prints an element of type 'statistics'
+void Free_Memory(int* arr, int n);
 
 
 void lab5() {
+
+	student_init_data students_init_data_arr[NUM];
+
+	printf("Please enter students data. \n\n");
+
+	for (int i = 0; i < NUM; i++) {
+		
+		printf("Enter id: ");
+		scanf_s("%d", &students_init_data_arr[i].id);
+		
+		printf("\nEnter number of exams: ");
+		scanf_s("%d", &students_init_data_arr[i].num_of_exams);
+		
+		int* grades = (int*)calloc(students_init_data_arr[i].num_of_exams, sizeof(int)); // FREE LATER!
+
+		printf("\nEnter grades: ");
+		for (int j = 0; j < students_init_data_arr[i].num_of_exams; j++) {
+			scanf_s("%d", &grades[j]);
+		}
+
+
+		students_init_data_arr[i].grades = *grades;
+
+	}
+
+
+
 
 }
